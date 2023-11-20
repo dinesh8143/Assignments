5a6138c8b06e6428c25d7859327b1fef27433bcb
dinesh8143 <30955698+dinesh8143@users.noreply.github.com>
2023-11-20 18:18:42 +0530

    Add files via upload

---
+++ PDineshPython-Assignement-Day1- Questions-1 (1).py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# coding: utf-8
+1.  Python Program to exchange the values of two numbers without using a temporary variable. 
+# In[1]:
+
+
+a=10
+b=8
+a,b=b,a
+
+2. Write a python code to replace all  "a"  with 2 for the given variable
+s = 'abc12321cba'
+# In[2]:
+
+
+s='abc12321cba'
+s=s.replace('a','2')
+print(s)
+
+3. Python code for to  remove first character in string?
+# In[22]:
+
+
+a='hello'
+a[1:]
+
+4. Concatenate two strings  
+First Input : "hello" 
+Second Input : "guvi"
+desired output :   "hello guvi"
+# In[3]:
+
+
+First = "hello"
+second="guvi"
+
+str3=" ".join([First, second]) 
+print(str3)
+
+5. Repeat the string "hello guvi" 3 times 
+desired output :  'hello guvi hello guvi hello guvi '
+# In[4]:
+
+
+First = "hello"
+second="guvi"
+
+str3=" ".join([First, second]*3) 
+
+print(str3)
+
+
+# In[ ]:
+
+
+6. Create 3 variables a1,a2,a3. Assign the same value " 4 " to all the variables in a single line of code.
+
+
+# In[19]:
+
+
+a1=a2=a3=4
+
+7. Calculate BMI of a person using the height and weight.
+BMI = weight / (height **2)
+
+# In[7]:
+
+
+height=input()
+weight=input()
+BMI = weight /float(height*height)
+str=BMI
+print(str)
+
+8.Write a python program to calculate the area of circle.
+# In[ ]:
+
+
+
+
+9.Calculate the simple interest
+simple_interest = p * n * r /100
+# In[12]:
+
+
+int main()
+{
+p=int[input()]
+n=int[input()]
+r=int[input()]
+m=(p*n*r)/100
+print(m)
+}
+
+10.Python code to calculate the square root of the given number(run time variable)
+# In[14]:
+
+
+import math  
+x = input()  
+y = math.sqrt(x)  
+print(y) 
+
+
+# In[ ]:
+
+
+
+
